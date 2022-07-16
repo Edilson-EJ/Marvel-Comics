bbc25e9fd4cd2e45085afb18ff10efc8a86ded8e
Edilson-EJ <edilsonjunior624@gmail.com>
2022-07-16 08:18:57 -0300

    Api de rotas do MAPS, implementção começada

--- maps/maps.js
+++ maps/maps.js
@@ -1,47 +1,30 @@
-var cap = document.getElementById("address");
-console.log(cap.value)
-
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: uluru,
+function initMap(){
+    const directionsService = new google.maps.directionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer({
+        draggable: false
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
+    const map = new google.Map(document.getElementById("map"),{
+        zoom: 7,
+        center: {lat: 41.85, lag: -87.65}
     });
-}
 
-window.initMap = initMap;
+    directionsRenderer.setMap(map);
 
-var geocoder;
-var map;
-function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var mapOptions = {
-    zoom: 8,
-    center: latlng
-    }
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
+    directionsService.route({
+        origin: 'Terminal Siqueira, Fortaleza-CE',
+        destination: 'Prefeitura De Jaguaribe, Jaguaribe-CE',
 
-function codeAddress() {
-    var address = document.getElementById('address');
-    geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status == 'OK') {
-            map.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
-        } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-    }
-});
+        travelMode: google.maps.travelMode.TRANSIT
+
+    }).the(response =>{
+        console.log({response});
+        directionsRenderer.setDirections(response);
+
+        //map.fitBounds( response.routes[0].bounds );
+        // renderPolyline(response.routes[0].overview_path, map);
+
+    }).catch(err => {
+        console.log({err});
+    });
+
 }
