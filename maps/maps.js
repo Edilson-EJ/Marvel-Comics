c9328da003f6214980728053cb3b02c87721d40c
Edilson-EJ <edilsonjunior624@gmail.com>
2022-07-16 21:37:28 -0300

    Tentado implementa o google API

--- maps/maps.js
+++ maps/maps.js
@@ -1,30 +1,46 @@
-function initMap(){
-    const directionsService = new google.maps.directionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer({
-        draggable: false
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+ function initMap() {
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const directionsService = new google.maps.DirectionsService();
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 7,
+      center: { lat:  -5.88667, lng: -38.6203},
+      disableDefaultUI: true,
     });
-    const map = new google.Map(document.getElementById("map"),{
-        zoom: 7,
-        center: {lat: 41.85, lag: -87.65}
-    });
-
+  
     directionsRenderer.setMap(map);
-
-    directionsService.route({
-        origin: 'Terminal Siqueira, Fortaleza-CE',
-        destination: 'Prefeitura De Jaguaribe, Jaguaribe-CE',
-
-        travelMode: google.maps.travelMode.TRANSIT
-
-    }).the(response =>{
-        console.log({response});
+    directionsRenderer.setPanel(document.getElementById("sidebar"));
+  
+    const control = document.getElementById("floating-panel");
+  
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+  
+    const onChangeHandler = function () {
+      calculateAndDisplayRoute(directionsService, directionsRenderer);
+    };
+  
+    document.getElementById("start").addEventListener("change", onChangeHandler);
+    document.getElementById("end").addEventListener("change", onChangeHandler);
+  }
+  
+  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    const start = document.getElementById("start").value;
+    const end = document.getElementById("end").value;
+  
+    directionsService
+      .route({
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING,
+      })
+      .then((response) => {
         directionsRenderer.setDirections(response);
-
-        //map.fitBounds( response.routes[0].bounds );
-        // renderPolyline(response.routes[0].overview_path, map);
-
-    }).catch(err => {
-        console.log({err});
-    });
-
-}
+      })
+      .catch((e) => window.alert("Directions request failed due to " + status));
+  }
+  
+  window.initMap = initMap;
